9b6826d502cd3ff6b5466e7f2eaa2ee531ede38e
Nicky Vermeersch <afotai@gmail.com>
2014-08-05 14:51:57 +0200

    Refactor to new guidelines and structure

--- make-debug.bat
+++
@@ -1,8 +0,0 @@
-SET oldDir=%CD%
-cd %appdata%\.minecraft\resourcepacks
-SET newDir=%CD%
-cd %oldDir%
-if not exist %newDir%\MCME-Sound-DEBUG mkdir %newDir%\MCME-Sound-DEBUG
-del %newdir%\MCME-Sound-DEBUG\*.* /s /q
-xcopy /y/s %oldDir%\src\main\meta\* %newDir%\MCME-Sound-DEBUG
-xcopy /y/s %oldDir%\src\main\pack\* %newDir%\MCME-Sound-DEBUG\assets\minecraft
